5d6d25341b0da41b60fc42727e4f14bbdafeb8db
berna <bernam1985@gmail.com>
2023-01-06 19:25:21 -0500

    Adding new details to cart

--- src/wapapp/CreatedCart.java
+++ src/wapapp/CreatedCart.java
@@ -5,4 +5,8 @@ public class CreatedCart {
     // shopping card
     // cart
     // .......
+
+    // this is adding new details to the new cart
+    // this is adding section of the cart
+    // add add add
 }
